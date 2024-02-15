af271de7ce7ad4afb4545be9eb7ae7c4a9fcde15
amanavi75 <amanavi75@gmail.com>
2024-02-15 23:35:51 +0530

    first commit

---
+++ src/streams/filter.java
@@ -0,0 +1,26 @@
+package streams;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class filter {
+  
+    public static void main(String[] args){
+    //    ArrayList<Integer> numberList = new ArrayList<>();
+    //    numberList.add(10);
+    //    numberList.add(20);
+    //    numberList.add(30);
+    //    numberList.add(40);
+
+    //* additional method to create list  */
+
+    List<Integer>numberList = Arrays.asList(10,20,30,40);
+
+    List<Integer>evenNum= new ArrayList<Integer>();
+
+    
+
+
+       
+    }
+    
+}
