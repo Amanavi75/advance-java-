6a0bdb092fb96c7d9ee7c8db53e9125c7f721141
amanavi75 <amanavi75@gmail.com>
2024-02-15 23:54:53 +0530

    added additional method

--- src/filter1.java
+++ src/filter1.java
@@ -1,7 +1,7 @@
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors; class filter1 {
+ class filter1 {
   
     public static void main(String[] args){
     //    ArrayList<Integer> numberList = new ArrayList<>();
@@ -14,15 +14,18 @@ import java.util.stream.Collectors; class filter1 {
 
     List<Integer>numberList = Arrays.asList(10,21,30,40);
 
-    List<Integer>evenNum= new ArrayList<Integer>();
+    // List<Integer>evenNum= new ArrayList<Integer>();
 
     //first of all we have to add collection to the streams 
 
-    evenNum=numberList.stream().filter(n->n%2==0).collect(Collectors.toList());
-    System.out.println(evenNum);
+    // evenNum=numberList.stream().filter(n->n%2==0).collect(Collectors.toList());
+    // System.out.println(evenNum);
     // numberList collection is added to list 
 
 
+    /* we can also use for each method */
+
+    numberList.stream().filter(n->n%2==0).forEach(n->System.out.println(n));
 
 
     
