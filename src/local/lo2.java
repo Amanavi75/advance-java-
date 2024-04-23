99a0f8c07e1153d78aaca352949f9f606274bb7a
amanavi75 <amanavi75@gmail.com>
2024-04-23 11:54:31 +0530

    added code for loacle

---
+++ src/local/lo2.java
@@ -0,0 +1,17 @@
+package local;
+
+import java.util.Locale;
+
+public class lo2 {
+    public static void main(String[] args) {
+        Locale en = new Locale("en","USA");
+        Locale fr = new Locale("fr","FR");
+        Locale es = new Locale("no","NO");
+        Locale svobj = new Locale("sv","SE");
+
+        System.out.println(en.getDisplayLanguage());
+        System.out.println(fr.getDisplayLanguage(fr));
+        System.out.println(es.getDisplayLanguage(es));
+        System.out.println(svobj.getDisplayLanguage(svobj));
+    }
+}
