1912f7fbc2c0380433c7b3de38c4f3627010d4ef
amanavi75 <amanavi75@gmail.com>
2024-04-26 09:05:43 +0530

    added second code for locale class

--- src/local/lo2.java
+++ src/local/lo2.java
@@ -8,10 +8,15 @@ public class lo2 {
         Locale fr = new Locale("fr","FR");
         Locale es = new Locale("no","NO");
         Locale svobj = new Locale("sv","SE");
+        Locale cn = new Locale("zh","cn");
+
 
         System.out.println(en.getDisplayLanguage());
         System.out.println(fr.getDisplayLanguage(fr));
         System.out.println(es.getDisplayLanguage(es));
         System.out.println(svobj.getDisplayLanguage(svobj));
+        System.out.println(cn.getDisplayLanguage());
+
+    
     }
 }
