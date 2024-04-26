8e6c8bca4704c0237f665d3cd1f27bc2c6c7ebf6
amanavi75 <amanavi75@gmail.com>
2024-04-26 10:26:34 +0530

    date instance fromatter with locale

---
+++ src/local/lo5.java
@@ -0,0 +1,29 @@
+package local;
+
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class lo5 {
+    static void printTime(Locale locale){
+        DateFormat formatter = DateFormat.getDateInstance(DateFormat.DEFAULT, locale);
+        Date currentDate = new Date();
+        String time = formatter.format(currentDate);
+
+        String date = formatter.format(currentDate);
+        //System.out.println(time + " in particular date Locale "+locale);
+        System.out.println(date+ "  in particular date Locale "+locale);
+
+    }
+    public static void main(String[] args) {
+        printTime(Locale.UK);
+        printTime(Locale.US);
+        printTime(Locale.FRANCE);
+        printTime(Locale.GERMANY);
+        printTime(Locale.ITALIAN);
+        printTime(Locale.CHINA);
+    }
+    
+    
+}
